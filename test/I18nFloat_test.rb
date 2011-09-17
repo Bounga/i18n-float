69731c40c8f7f48234b4729a34fbc931b5ee83f8
Nicolas Cavigneaux <nico@bounga.org>
2011-09-17 11:23:07 +0200

    Move tests to dummy application

--- test/I18nFloat_test.rb
+++ test/I18nFloat_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class I18nFloatTest < ActiveSupport::TestCase
-  test "1200" do
-    assert_equal 1200.00, Marathon.new(:duration => "1200").duration
-  end
-  
-  test "1200.00" do
-    assert_equal 1200.00, Marathon.new(:duration => "1200.00").duration
-  end
-  
-  test "1200,00" do
-    assert_equal 1200.00, Marathon.new(:duration => "1200,00").duration
-  end
-  
-  test "1 200,00" do
-    assert_equal 1200.00, Marathon.new(:duration => "1 200,00").duration
-  end
+
 end
--- test/dummy/test/unit/marathon_test.rb
+++ test/dummy/test/unit/marathon_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MarathonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "1200" do
+    assert_equal 1200.00, Marathon.new(:duration => "1200").duration
+  end
+  
+  test "1200.00" do
+    assert_equal 1200.00, Marathon.new(:duration => "1200.00").duration
+  end
+  
+  test "1200,00" do
+    assert_equal 1200.00, Marathon.new(:duration => "1200,00").duration
+  end
+  
+  test "1 200,00" do
+    assert_equal 1200.00, Marathon.new(:duration => "1 200,00").duration
+  end
 end
